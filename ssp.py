4b7cb545f13fccccdebf044d74699a2b70724332
Kevvee <Kevjoh.044@gmail.com>
2022-03-28 13:09:07 +0200

    ✅ början

--- ssp.py
+++ ssp.py
@@ -1,6 +1,20 @@
-print("Välkommen till Sten, Sax, Påse!")
-input("[1] Spela\n[2] Exit\n")
-svar = ''
-while svar != '':
-    if svar == ("1"):
-        print('startar spelet!')
+#print("Välkommen till Sten, Sax, Påse!")
+#svar = input("[1] Spela\n[2] Avsluta\n")
+
+#if svar == "1":
+    #print("Du valde att spela spelet!")
+
+#if svar == "2":
+    #print("Du valde att avsluta spelet!")
+    #exit()
+
+#namn = input("Vad vill du heta? ")
+
+#print(f"Välkommen till spelet {namn}!")
+
+gamer = input("Välj \n [1]sten\n [2]sax\n [3]påse\n")
+
+import random
+bot = random.randint (1,3)
+if bot == 1:
+    print("hej")
